616b8876eb9fb3d19b3fdc2f7385d1662fd4d736
Kelvin Ka-Kwok Lin <linkk4@mcmaster.ca>
2016-10-15 23:30:12 -0400

    'Fixed Commiting Message Problem'

--- commit.sh
+++ commit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $# -lt 1]; then
+if [ $# -lt 1 ]; then
     echo $0: usage: commit branch-name [commit-message]
 	exit 1
 fi
@@ -9,10 +9,11 @@ branch=$1
 
 git add .
 
-if [$# -eq 2]; then
+if [ $# -eq 2 ]; then
 	git commit -m "$2"
 else
 	git commit -m 'End of Day Commit'
+
 fi
 
 git push -u origin $branch
