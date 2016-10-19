eeef7a6fe6408e2f9206e1b292ab803a81a470c0
liu465@mcmaster.ca <liu465@mcmaster.ca>
2016-10-18 20:26:11 -0400

    fix some bugs

--- src/JavaScript.js
+++ src/JavaScript.js
@@ -19,13 +19,13 @@
         Y[u] = 300 * Math.random();
     }
 
-    function rectan(x, y, x1, y1, x2, y2, x3, y3) { //polar coordinates will be converted to Cartesian coordinates to fit x,y,x1,y1...)
+    function rectan(x, y, x2, y2, x3, y3, x4, y4, x5, y5, x6, y6) { //polar coordinates will be converted to Cartesian coordinates to fit x,y,x1,y1...)
         c.moveTo(x, y);
-        c.lineTo(x1, y1);
         c.lineTo(x2, y2);
         c.lineTo(x3, y3);
-
-
+        c.lineTo(x4, y4);
+        c.lineTo(x5, y5);
+        c.lineTo(x6, y6);
         c.closePath();
         c.fillStyle = "rgb(200,0,0)";                                   //set color
         c.stroke();
@@ -102,7 +102,7 @@
     function draw() {
         c.clearRect(0, 0, canvas.width, canvas.height);
         c.beginPath();
-        rectan(0, 0, x+100, y+50, x+150, y+50, x+20, y+40);
+        rectan(0 + x, 300 + y, 0 + x, 200 + y, 50 + x, 200 + y, 100 + x, 300 + y, 150 + x, 100 + y, 200 + x, 300 + y);
         c.fill();
         x += 8;                                                  //x(speed) will be determinded by GA 
         //y += 8;
