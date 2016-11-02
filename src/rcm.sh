23e1bed436c4ec68b9e292f79f20d4e08f391ee4
Kelvin Ka-Kwok Lin <linkk4@mcmaster.ca>
2016-11-02 09:04:40 -0400

    Scrapped PoC, Starting on Real Code

--- src/rcm.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-node Car_Model.js > cm_out
-
-#The following line of code was obtained from Indiana University, available at:
-#https://kb.iu.edu/d/acux
-perl -p -e 's/\n/\r\n/' < cm_out > cm_out.txt
