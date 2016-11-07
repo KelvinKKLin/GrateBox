13b144817efbeab82bd572bb4231f39faee20f06
liu465@mcmaster.ca <liu465@mcmaster.ca>
2016-11-07 03:14:19 -0500

    car with polygon and constrained wheels

--- src/AsimpleCar.js
+++ src/AsimpleCar.js
@@ -12,58 +12,269 @@ function init() {
        , b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape
        , b2CircleShape = Box2D.Collision.Shapes.b2CircleShape
        , b2DebugDraw = Box2D.Dynamics.b2DebugDraw
+       , b2RevoluteJointDef = Box2D.Dynamics.Joints.b2RevoluteJointDef
+       , b2Joint = Box2D.Dynamics.b2Joint
     ;
 
     world = new b2World(
           new b2Vec2(0, 9.8)    //gravity
        , true                 //allow sleep
     );
+    var worldScale = 60;
+
+    drawPolygon();
+    createRoad(world, 0, 15, 150);
+
+    /*var fixDef = new b2FixtureDef;
+    fixDef.density = 1.0;
+    fixDef.friction = 0.5;
+    fixDef.restitution = 0.2;
+    var bodyDef = new b2BodyDef;
+    bodyDef.type = b2Body.b2_staticBody;
+    bodyDef.position.x = 9;
+    bodyDef.position.y = 13;
+    fixDef.shape = new b2PolygonShape;
+    fixDef.shape.SetAsBox(100, 0.5);
+    world.CreateBody(bodyDef).CreateFixture(fixDef);*/
+
+    function drawPolygon() {
+        //**************************polygon1**************************//
+        var polygon1 = new b2PolygonShape;
+        var polygonFix1 = new b2FixtureDef;
+        polygonFix1.shape = polygon1;
+        var points = [{ x: 0, y: 0 }, { x: 2, y: -1 }, { x: 4, y: 0 }];
+
+        for (var i = 0; i < 3; i++) {
+            var vec = new b2Vec2();
+            vec.Set(points[i].x, points[i].y);
+            points[i] = vec;
+        }
+
+        polygonFix1.shape.SetAsArray(points, points.length);
+        polygonFix1.density = 5;
+        polygonFix1.friction = 3;
+        polygonFix1.filter.groupInedx = -1;
+        polygonFix1.restitution = 0.3;
+
+        //**************************polygon2**************************//
+        var polygon2 = new b2PolygonShape;
+        var polygonFix2 = new b2FixtureDef;
+        polygonFix2.shape = polygon2;
+        var points = [{ x: 0, y: 0 }, { x: -1, y: -2 }, { x: 2, y: -1 }];
 
 
-    this.bodyDef = new b2BodyDef;
-    this.bodyDef.type = b2Body.b2_dynamicBody;
-    this.PolygonfixDef = new b2FixtureDef;
-    this.PolygonfixDef.shape = new b2PolygonShape;
-    this.PolygonfixDef.density = 1.0;
-    this.PolygonfixDef.friction = 0.5;
-    this.PolygonfixDef.restitution = 0.2;
-  
-    var points = [{ x: 0, y: 0 }, { x: 1, y: 0 }, { x: 0, y: 2 }, { x: 2, y: 2 }];
+        for (var i = 0; i < 3; i++) {
+            var vec = new b2Vec2();
+            vec.Set(points[i].x, points[i].y);
+            points[i] = vec;
+        }
+        polygonFix2.shape.SetAsArray(points, points.length);
+        polygonFix2.density = 5;
+        polygonFix2.friction = 3;
+        polygonFix2.filter.groupInedx = -1;
+        polygonFix2.restitution = 0.3;
 
-    for (var i = 0; i < 4; i++) {
-        var vec = new b2Vec2();
-        vec.Set(points[i].x, points[i].y);
-        points[i] = vec;
+        //**************************polygon3**************************//
+        var polygon3 = new b2PolygonShape;
+        var polygonFix3 = new b2FixtureDef;
+        polygonFix3.shape = polygon3;
+        var points = [{ x: 0, y: 0 }, { x: -2, y: 0.5 }, { x: -1, y: -2 }];
+
+
+        for (var i = 0; i < 3; i++) {
+            var vec = new b2Vec2();
+            vec.Set(points[i].x, points[i].y);
+            points[i] = vec;
+        }
+
+        polygonFix3.shape.SetAsArray(points, points.length);
+        polygonFix3.density = 5;
+        polygonFix3.friction = 3;
+        polygonFix3.filter.groupInedx = -1;
+        polygonFix3.restitution = 0.3;
+
+        //**************************polygon4**************************//
+        var polygon4 = new b2PolygonShape;
+        var polygonFix4 = new b2FixtureDef;
+        polygonFix4.shape = polygon4;
+        var points = [{ x: 0, y: 0 }, { x: -0.25, y: 0.25 }, { x: -2, y: 0.5 }];
+
+
+        for (var i = 0; i < 3; i++) {
+            var vec = new b2Vec2();
+            vec.Set(points[i].x, points[i].y);
+            points[i] = vec;
+        }
+        polygonFix4.shape.SetAsArray(points, points.length);
+        polygonFix4.density = 5;
+        polygonFix4.friction = 3;
+        polygonFix4.filter.groupInedx = -1;
+        polygonFix4.restitution = 0.3;
+
+        //**************************polygon5**************************//
+        var polygon5 = new b2PolygonShape;
+        var polygonFix5 = new b2FixtureDef;
+        polygonFix5.shape = polygon5;
+        var points = [{ x: 0, y: 0 }, { x: 0.75, y: 0.5 }, { x: -0.25, y: 0.25 }];
+
+
+        for (var i = 0; i < 3; i++) {
+            var vec = new b2Vec2();
+            vec.Set(points[i].x, points[i].y);
+            points[i] = vec;
+        }
+        polygonFix5.shape.SetAsArray(points, points.length);
+        polygonFix5.density = 5;
+        polygonFix5.friction = 3;
+        polygonFix5.filter.groupInedx = -1;
+        polygonFix5.restitution = 0.3;
+        //**************************polygon6**************************//
+        var polygon6 = new b2PolygonShape;
+        var polygonFix6 = new b2FixtureDef;
+        polygonFix6.shape = polygon6;
+        var points = [{ x: 0, y: 0 }, { x: 4, y: 0 }, { x: 0.75, y: 0.5 }];
+
+
+        for (var i = 0; i < 3; i++) {
+            var vec = new b2Vec2();
+            vec.Set(points[i].x, points[i].y);
+            points[i] = vec;
+        }
+        polygonFix6.shape.SetAsArray(points, points.length);
+        polygonFix6.density = 5;
+        polygonFix6.friction = 3;
+        polygonFix6.filter.groupInedx = -1;
+        polygonFix6.restitution = 0.3;
+
+
+        var carBodyDef = new b2BodyDef;
+        carBodyDef.type = b2Body.b2_dynamicBody;
+        carBodyDef.position.Set(320 / worldScale, 100 / worldScale);
+
+
+        var car = world.CreateBody(carBodyDef);
+        car.CreateFixture(polygonFix1);
+        car.CreateFixture(polygonFix2);
+        car.CreateFixture(polygonFix3);
+        car.CreateFixture(polygonFix4);
+        car.CreateFixture(polygonFix5);
+        car.CreateFixture(polygonFix6);
+
+
+        addEventListener(event.ENTER_FRAME, update);
+
+
+
+
+        var wheelshape = new b2CircleShape(40 / worldScale);
+        var wheelFixture = new b2FixtureDef;
+        wheelFixture.density = 1;
+        wheelFixture.friction = 3;
+        wheelFixture.filter.groupInedx = -1;
+        wheelFixture.restitution = 0.1;
+        wheelFixture.shape = wheelshape;
+        var wheelshape2 = new b2CircleShape(70 / worldScale);
+        var wheelFixture2 = new b2FixtureDef;
+        wheelFixture2.density = 1;
+        wheelFixture2.friction = 3;
+        wheelFixture2.filter.groupInedx = -1;
+        wheelFixture2.restitution = 0.1;
+        wheelFixture2.shape = wheelshape2;
+        var wheelbodyDef = new b2BodyDef;
+        wheelbodyDef.type = b2Body.b2_dynamicBody;
+        wheelbodyDef.position.Set(car.GetWorldCenter().x - (80 / worldScale), car.GetWorldCenter().y + (65 / worldScale));
+        var rearwheel = world.CreateBody(wheelbodyDef);
+        rearwheel.CreateFixture(wheelFixture);
+        wheelbodyDef.position.Set(car.GetWorldCenter().x + (100 / worldScale), car.GetWorldCenter().y + (65 / worldScale));
+        var frontwheel = world.CreateBody(wheelbodyDef);
+        frontwheel.CreateFixture(wheelFixture2);
+        addEventListener(event.ENTER_FRAME, update);
+
+        var joint_def = new b2RevoluteJointDef();
+        joint_def.bodyA = rearwheel;
+        joint_def.bodyB = car;
+        joint_def.localAnchorA = new b2Vec2(0, 0);
+        joint_def.localAnchorB = new b2Vec2(-2, 0.5);
+        joint_def.maxMotorTorque = 30;
+        joint_def.motorSpeed = -50;
+        joint_def.enableMotor = true;
+        world.CreateJoint(joint_def);
+
+        var joint_def = new b2RevoluteJointDef();
+        joint_def.bodyA = frontwheel;
+        joint_def.bodyB = car;
+        joint_def.localAnchorA = new b2Vec2(0, 0);
+        joint_def.localAnchorB = new b2Vec2(4, 0);
+        joint_def.maxMotorTorque = 30;
+        joint_def.motorSpeed = -50;
+        joint_def.enableMotor = true;
+        world.CreateJoint(joint_def);
+
+
+
     }
 
-    this.PolygonfixDef.shape.SetAsArray(points, points.length);
+    function createBox(world, x, y, width, height, angle) {
+        var body_def = new b2BodyDef();
+        var fix_def = new b2FixtureDef();
 
-    this.bodyDef.position.x = 5;
-    this.bodyDef.position.y = 5;
-    this.world.CreateBody(this.bodyDef).CreateFixture(this.PolygonfixDef);
+        fix_def.density = 1.0;
+        fix_def.friction = 0.5;
+        fix_def.restitution = 0;
 
-    this.CircleFixDef = new b2FixtureDef;
-    this.CircleFixDef.shape = new b2CircleShape(1);
-    this.CircleFixDef.density = 1.0;
-    this.CircleFixDef.friction = 0.5;
-    this.CircleFixDef.restitution = 0.2;
-    this.world.CreateBody(this.bodyDef).CreateFixture(this.CircleFixDef);
+        fix_def.shape = new b2PolygonShape();
 
-    this.Circle1FixDef = new b2FixtureDef;
-    this.Circle1FixDef.shape = new b2CircleShape(1);
-    this.Circle1FixDef.density = 1.0;
-    this.Circle1FixDef.friction = 0.5;
-    this.Circle1FixDef.restitution = 0.2;
-    this.world.CreateBody(this.bodyDef).CreateFixture(this.Circle1FixDef);
+        fix_def.shape.SetAsBox(width, height);
+
+        body_def.position.Set(x, y);
+
+        var body = world.CreateBody(body_def);
+        var fixture = body.CreateFixture(fix_def);
+        console.log(body.GetPosition());
+        body.SetPositionAndAngle(body.GetPosition(), angle);
+        return body;
+    }
+
+    function createRoad(world, startX, startY, maxNumberOfTiles) {
+        var lastX = startX;
+        var lastY = startY;
+        var lastHeight = 0.1
+        var angle = 0.2;//0.78;
+        for (var i = 0; i < maxNumberOfTiles; i++) {
+            lastTile = createBox(world, lastX, lastY, 0.78, lastHeight, angle);
+            angle = angle * -1;
+            lastX = lastX + 1;
+        }
+    }
 
 
-    this.body2Def = new b2BodyDef;
-    this.body2Def.type = b2Body.b2_staticBody;
-    this.fixDef = new b2FixtureDef;
-    this.fixDef.shape = new b2PolygonShape;
-    this.fixDef.shape.SetAsBox(5, 0.25);
-    this.body2Def.position.Set(5, 20);
-    this.world.CreateBody(this.body2Def).CreateFixture(this.fixDef);
+
+
+
+
+
+    //Create standard boxes of given height , width at x,y
+    
+
+    /*function drawWheel() {
+        var wheelshape = new b2CircleShape(40/worldScale);
+        var wheelFixture = new b2FixtureDef;
+        wheelFixture.density = 1;
+        wheelFixture.friction = 3;
+        wheelFixture.filter.groupInedx = -1;
+        wheelFixture.restitution = 0.1;
+        wheelFixture.shape = wheelshape;
+        var wheelbodyDef = new b2BodyDef;
+        wheelbodyDef.type = b2Body.b2_dynamicBody;
+        wheelbodyDef.position.Set(60 / worldScale, 65 / worldScale);
+        var rearwheel = world.CreateBody(wheelbodyDef);
+        rearwheel.CreateFixture(wheelFixture);
+        wheelbodyDef.position.Set(75 / worldScale, 65 / worldScale);
+        var frontwheel = world.CreateBody(wheelbodyDef);
+        frontwheel.CreateFixture(wheelFixture);
+        addEventListener(event.ENTER_FRAME, update);
+
+    };*/
 
 
 
